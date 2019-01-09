f6e2203402a8f3ea9d32a8f4c01bf4dc75e85703
ileana <ileana.oneata94@e-uvt.ro>
2019-01-09 11:52:26 +0200

    Comments removed

--- DesignPatterns/src/patterns/Client.java
+++ DesignPatterns/src/patterns/Client.java
@@ -9,6 +9,13 @@ package patterns;
 import patterns.command.Command;
 import patterns.command.OpenCommand;
 import patterns.command.StatisticsCommand;
+import patterns.model.Book;
+import patterns.model.Display;
+import patterns.model.Image;
+import patterns.model.ImageProxy;
+import patterns.model.Paragraph;
+import patterns.model.Section;
+import patterns.model.Table;
 
 /**
  * @author <a href="mailto:bario@ssi-schaefer-noell.com">bario</a>
@@ -17,35 +24,35 @@ import patterns.command.StatisticsCommand;
 
 public class Client {
 
-  public static void main(String[] args) {
-    //    Section cap1 = new Section("Capitolul 1");
-    //    Paragraph p1 = new Paragraph("Paragraph 1");
-    //    cap1.add(p1);
-    //    Paragraph p2 = new Paragraph("Paragraph 2");
-    //    cap1.add(p2);
-    //    Paragraph p3 = new Paragraph("Paragraph 3");
-    //    cap1.add(p3);
-    //    Paragraph p4 = new Paragraph("Paragraph 4");
-    //    cap1.add(p4);
-    //    cap1.add(new ImageProxy("ImageOne"));
-    //    cap1.add(new Image("ImageTwo"));
-    //    cap1.add(new Paragraph("Some text"));
-    //    cap1.add(new Table("Table 1"));
-    //
-    //    Section cap2 = new Section("Capitolul 1");
-    //    Paragraph p12 = new Paragraph("Paragraph 1");
-    //    cap2.add(p12);
-    //    Book book = new Book("Cartea mea");
-    //    book.add(cap1);
-    //    book.add(cap2);
-    Command openCommand = new OpenCommand();
-    openCommand.execute();
+	public static void main(String[] args) {
+		Section cap1 = new Section("Capitolul 1");
+		Paragraph p1 = new Paragraph("Paragraph 1");
+		cap1.add(p1);
+		Paragraph p2 = new Paragraph("Paragraph 2");
+		cap1.add(p2);
+		Paragraph p3 = new Paragraph("Paragraph 3");
+		cap1.add(p3);
+		Paragraph p4 = new Paragraph("Paragraph 4");
+		cap1.add(p4);
+		cap1.add(new ImageProxy("ImageOne"));
+		cap1.add(new Image("ImageTwo"));
+		cap1.add(new Paragraph("Some text"));
+		cap1.add(new Table("Table 1"));
 
-    Command statisticsCommand = new StatisticsCommand();
-    statisticsCommand.execute();
+		Section cap2 = new Section("Capitolul 1");
+		Paragraph p12 = new Paragraph("Paragraph 1");
+		cap2.add(p12);
+		Book book = new Book("Cartea mea");
+		book.add(cap1);
+		book.add(cap2);
+		Command openCommand = new OpenCommand();
+		openCommand.execute();
 
-    //    Display display = new Display();
-    //    cap1.accept(display);
-  }
+		Command statisticsCommand = new StatisticsCommand();
+		statisticsCommand.execute();
+
+		Display display = new Display();
+		cap1.accept(display);
+	}
 
 }
